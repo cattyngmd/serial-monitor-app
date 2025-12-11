92ed822d23fbafbccefdf4081ccf845894c20809
cattyn <cvenby@gmail.com>
2025-12-11 05:02:45 +0300

    refactor: removed unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("edu.sc.seis.launch4j") version "3.0.3"
     id("java")
     id("application")
 }
@@ -15,19 +14,6 @@ repositories {
 dependencies {
     implementation("com.fazecast:jSerialComm:[2.0.0,3.0.0)")
     implementation("com.profesorfalken:jSensors:2.2.1")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-launch4j {
-    mainClassName = "dev.cattyn.serialmonitorapp.Launcher" // Replace with your main class
-    outfile = "serial-monitor.exe"
-
-//    icon = 'src/main/resources/app_icon.ico' // Optional: path to your icon file
-//    jrePath = 'jre' // Optional: path to a bundled JRE relative to the executable
-    // ... other Launch4j configurations
 }
 
 application {
